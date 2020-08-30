cac12060d28c1ed5bf7cdcf3161eb5e6cae70dbc
JoalbertMilano <joalbertmilano@gmail.com>
2020-08-30 14:08:34 -0400

    Añadiendo maquetado de Home

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Validate from './pages/ValidateAccount'
 import Waiting from './pages/WaitingActivation'
+import Home from './pages/Home'
 
 export default () =>{
     return(
-        <Validate />
+        <Home />
     )
 }
